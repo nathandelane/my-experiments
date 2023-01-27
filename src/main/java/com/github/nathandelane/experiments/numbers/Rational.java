29a7f51ba28663f756cd677684d6c8d3566656d6
Nathan Lane <nathan.lane@overstock.com>
2023-01-27 12:08:43 -0700

    Fix fromDouble for >0 whole parts, add more tests for coverage.

--- src/main/java/com/github/nathandelane/experiments/numbers/Rational.java
+++ src/main/java/com/github/nathandelane/experiments/numbers/Rational.java
@@ -1,12 +1,10 @@
 package com.github.nathandelane.experiments.numbers;
 
-import java.math.BigDecimal;
 import java.util.Map;
 
 public class Rational {
 
-  private static final Map<String, Rational> RATIONAL_CACHE = Map.ofEntries(
-    Map.entry("0", new Rational(1, 1)),
+  private static final Map<String, Rational> DECIMAL_TO_RATIONAL_CACHE = Map.ofEntries(
     Map.entry("5", new Rational(1, 2)),
     Map.entry("3333333333333333", new Rational(1, 3)),
     Map.entry("25", new Rational(1, 4)),
@@ -16,96 +14,28 @@ public class Rational {
     Map.entry("125", new Rational(1, 8)),
     Map.entry("1111111111111111", new Rational(1, 9)),
     Map.entry("1", new Rational(1, 10)),
-    Map.entry("09090909090909091", new Rational(1, 11)),
-    Map.entry("08333333333333333", new Rational(1, 12)),
-    Map.entry("07692307692307693", new Rational(1, 13)),
-    Map.entry("07142857142857142", new Rational(1, 14)),
-    Map.entry("06666666666666667", new Rational(1, 15)),
-    Map.entry("0625", new Rational(1, 16)),
-    Map.entry("058823529411764705", new Rational(1, 17)),
-    Map.entry("05555555555555555", new Rational(1, 18)),
-    Map.entry("05263157894736842", new Rational(1, 19)),
-    Map.entry("05", new Rational(1, 20)),
-    Map.entry("047619047619047616", new Rational(1, 21)),
-    Map.entry("045454545454545456", new Rational(1, 22)),
-    Map.entry("043478260869565216", new Rational(1, 23)),
-    Map.entry("041666666666666664", new Rational(1, 24)),
-    Map.entry("04", new Rational(1, 25)),
-    Map.entry("038461538461538464", new Rational(1, 26)),
-    Map.entry("037037037037037035", new Rational(1, 27)),
-    Map.entry("03571428571428571", new Rational(1, 28)),
-    Map.entry("034482758620689655", new Rational(1, 29)),
-    Map.entry("03333333333333333", new Rational(1, 30)),
-    Map.entry("03225806451612903", new Rational(1, 31)),
-    Map.entry("03125", new Rational(1, 32)),
-    Map.entry("030303030303030304", new Rational(1, 33)),
-    Map.entry("029411764705882353", new Rational(1, 34)),
-    Map.entry("02857142857142857", new Rational(1, 35)),
-    Map.entry("027777777777777776", new Rational(1, 36)),
-    Map.entry("02702702702702703", new Rational(1, 37)),
-    Map.entry("02631578947368421", new Rational(1, 38)),
-    Map.entry("02564102564102564", new Rational(1, 39)),
-    Map.entry("025", new Rational(1, 40)),
-    Map.entry("024390243902439025", new Rational(1, 41)),
-    Map.entry("023809523809523808", new Rational(1, 42)),
-    Map.entry("023255813953488372", new Rational(1, 43)),
-    Map.entry("022727272727272728", new Rational(1, 44)),
-    Map.entry("022222222222222223", new Rational(1, 45)),
-    Map.entry("021739130434782608", new Rational(1, 46)),
-    Map.entry("02127659574468085", new Rational(1, 47)),
-    Map.entry("020833333333333332", new Rational(1, 48)),
-    Map.entry("02040816326530612", new Rational(1, 49)),
-    Map.entry("02", new Rational(1, 50)),
-    Map.entry("0196078431372549", new Rational(1, 51)),
-    Map.entry("019230769230769232", new Rational(1, 52)),
-    Map.entry("018867924528301886", new Rational(1, 53)),
-    Map.entry("018518518518518517", new Rational(1, 54)),
-    Map.entry("01818181818181818", new Rational(1, 55)),
-    Map.entry("017857142857142856", new Rational(1, 56)),
-    Map.entry("017543859649122806", new Rational(1, 57)),
-    Map.entry("017241379310344827", new Rational(1, 58)),
-    Map.entry("01694915254237288", new Rational(1, 59)),
-    Map.entry("016666666666666666", new Rational(1, 60)),
-    Map.entry("01639344262295082", new Rational(1, 61)),
-    Map.entry("016129032258064516", new Rational(1, 62)),
-    Map.entry("015873015873015872", new Rational(1, 63)),
-    Map.entry("015625", new Rational(1, 64)),
-    Map.entry("015384615384615385", new Rational(1, 65)),
-    Map.entry("015151515151515152", new Rational(1, 66)),
-    Map.entry("014925373134328358", new Rational(1, 67)),
-    Map.entry("014705882352941176", new Rational(1, 68)),
-    Map.entry("014492753623188406", new Rational(1, 69)),
-    Map.entry("014285714285714285", new Rational(1, 70)),
-    Map.entry("014084507042253521", new Rational(1, 71)),
-    Map.entry("013888888888888888", new Rational(1, 72)),
-    Map.entry("0136986301369863", new Rational(1, 73)),
-    Map.entry("013513513513513514", new Rational(1, 74)),
-    Map.entry("013333333333333334", new Rational(1, 75)),
-    Map.entry("013157894736842105", new Rational(1, 76)),
-    Map.entry("012987012987012988", new Rational(1, 77)),
-    Map.entry("01282051282051282", new Rational(1, 78)),
-    Map.entry("012658227848101266", new Rational(1, 79)),
-    Map.entry("0125", new Rational(1, 80)),
-    Map.entry("012345679012345678", new Rational(1, 81)),
-    Map.entry("012195121951219513", new Rational(1, 82)),
-    Map.entry("012048192771084338", new Rational(1, 83)),
-    Map.entry("011904761904761904", new Rational(1, 84)),
-    Map.entry("011764705882352941", new Rational(1, 85)),
-    Map.entry("011627906976744186", new Rational(1, 86)),
-    Map.entry("011494252873563218", new Rational(1, 87)),
-    Map.entry("011363636363636364", new Rational(1, 88)),
-    Map.entry("011235955056179775", new Rational(1, 89)),
-    Map.entry("011111111111111112", new Rational(1, 90)),
-    Map.entry("01098901098901099", new Rational(1, 91)),
-    Map.entry("010869565217391304", new Rational(1, 92)),
-    Map.entry("010752688172043012", new Rational(1, 93)),
-    Map.entry("010638297872340425", new Rational(1, 94)),
-    Map.entry("010526315789473684", new Rational(1, 95)),
-    Map.entry("010416666666666666", new Rational(1, 96)),
-    Map.entry("010309278350515464", new Rational(1, 97)),
-    Map.entry("01020408163265306", new Rational(1, 98)),
-    Map.entry("010101010101010102", new Rational(1, 99)),
-    Map.entry("01", new Rational(1, 100))
+    Map.entry("6666666666666666", new Rational(2, 3)),
+    Map.entry("4", new Rational(2, 5)),
+    Map.entry("2857142857142857", new Rational(2, 7)),
+    Map.entry("2222222222222222", new Rational(2, 9)),
+    Map.entry("75", new Rational(3, 4)),
+    Map.entry("6", new Rational(3, 5)),
+    Map.entry("42857142857142855", new Rational(3, 7)),
+    Map.entry("375", new Rational(3, 8)),
+    Map.entry("3", new Rational(3, 10)),
+    Map.entry("8", new Rational(4, 5)),
+    Map.entry("5714285714285714", new Rational(4, 7)),
+    Map.entry("4444444444444444", new Rational(4, 9)),
+    Map.entry("8333333333333334", new Rational(5, 6)),
+    Map.entry("7142857142857143", new Rational(5, 7)),
+    Map.entry("625", new Rational(5, 8)),
+    Map.entry("5555555555555556", new Rational(5, 9)),
+    Map.entry("8571428571428571", new Rational(6, 7)),
+    Map.entry("875", new Rational(7, 8)),
+    Map.entry("7777777777777778", new Rational(7, 9)),
+    Map.entry("7", new Rational(7, 10)),
+    Map.entry("8888888888888888", new Rational(8, 9)),
+    Map.entry("9", new Rational(9, 10))
   );
 
   private final long numerator;
@@ -113,8 +43,16 @@ public class Rational {
   private final long denominator;
 
   public Rational(final long numerator, final long denominator) {
-    this.numerator = numerator;
-    this.denominator = denominator;
+    if (denominator == 0) throw new ArithmeticException("Division by zero not allowed.");
+
+    if (denominator < 0) {
+      this.denominator = (denominator * -1);
+      this.numerator = (numerator * -1);
+    }
+    else {
+      this.denominator = denominator;
+      this.numerator = numerator;
+    }
   }
 
   public double asDouble() {
@@ -226,13 +164,15 @@ public class Rational {
   }
 
   public static Rational fromDouble(final double value) {
-    final String[] parts = Double.toString(value).split("[\\.]{1}");
-    final long wholePart = Long.parseLong(parts[0]);
-    final String decimalPart = parts[1];
-    final long longDecimalPart = Long.parseLong(decimalPart);
+    final String valueAsString = Double.toString(value);
+    final int indexOfDecimal = valueAsString.indexOf('.');
+    final String wholePartAsString = valueAsString.substring(0, indexOfDecimal);
+    final long wholePart = Long.parseLong(wholePartAsString);
+    final String decimalPartAsString = valueAsString.substring(indexOfDecimal + 1);
+    final long decimalPart = Long.parseLong(decimalPartAsString);
 
-    if (longDecimalPart > 0) {
-      final Rational fractionalPart = RATIONAL_CACHE.get(decimalPart);
+    if (decimalPart > 0) {
+      final Rational fractionalPart = DECIMAL_TO_RATIONAL_CACHE.get(decimalPartAsString);
 
       if (fractionalPart != null) {
         if (wholePart == 0) {
@@ -240,15 +180,19 @@ public class Rational {
         }
         else {
           final long wholePartTimesDenominator = (wholePart * fractionalPart.denominator);
+          final Rational wholePartAsRational = new Rational(wholePartTimesDenominator, fractionalPart.denominator);
 
-          return new Rational(wholePartTimesDenominator, fractionalPart.denominator);
+          return wholePartAsRational.add(fractionalPart);
         }
       }
       else {
-        final int lengthOfDecimalPart = decimalPart.length();
+        final int lengthOfDecimalPart = decimalPartAsString.length();
         final long denominator = Math.round(Math.pow(10.0, (double) lengthOfDecimalPart));
+        final long wholePartTimesDenominator = (wholePart * denominator);
+        final Rational wholePartAsRational = new Rational(wholePartTimesDenominator, denominator);
+        final Rational decimalPartAsRational = new Rational(decimalPart, denominator);
 
-        return new Rational(longDecimalPart, denominator);
+        return wholePartAsRational.add(decimalPartAsRational).reduce();
       }
     }
 
--- src/test/java/com/github/nathandelane/experiments/numbers/RationalsTests.java
+++ src/test/java/com/github/nathandelane/experiments/numbers/RationalsTests.java
@@ -1,11 +1,35 @@
 package com.github.nathandelane.experiments.numbers;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
 
 public class RationalsTests {
+
+  @Test
+  public void testConstructor1() {
+    final Rational r = new Rational(3, -4);
+    final Rational expected = new Rational(-3, 4);
+
+    assertEquals(expected, r);
+  }
+
+  @Test
+  public void testConstructor2() {
+    final Rational r = new Rational(-3, -4);
+    final Rational expected = new Rational(3, 4);
+
+    assertEquals(expected, r);
+  }
+
+  @Test(expected = ArithmeticException.class)
+  public void testConstructorZeroInDivisor() {
+    final Rational r = new Rational(1, 0);
+  }
 
   @Test
   public void testFromDouble() {
@@ -48,10 +72,30 @@ public class RationalsTests {
   }
 
   @Test
+  public void testFromDouble5() {
+    final double d = 3.1415;
+    final Rational r = Rational.fromDouble(d);
+    final Rational expected = new Rational(6283, 2000);
+
+    assertNotNull(r);
+    assertEquals(expected, r);
+  }
+
+  @Test
   public void testFromDoubleWholeNumber() {
     final double d = 24.0;
     final Rational r = Rational.fromDouble(d);
     final Rational expected = new Rational(24, 1);
+
+    assertNotNull(r);
+    assertEquals(expected, r);
+  }
+
+  @Test
+  public void testFromDoubleWholeAndDecimal() {
+    final double d = 3.125;
+    final Rational r = Rational.fromDouble(d);
+    final Rational expected = new Rational(25, 8);
 
     assertNotNull(r);
     assertEquals(expected, r);
@@ -99,6 +143,134 @@ public class RationalsTests {
 
     assertNotNull(r);
     assertEquals(expected, r);
+  }
+
+  @Test
+  public void testSubtract1() {
+    final Rational r1 = Rational.fromLong(1);
+    final Rational r2 = new Rational(1, 3);
+    final Rational r = r1.subtract(r2);
+    final Rational expected = new Rational(2, 3);
+
+    assertNotNull(r);
+    assertEquals(expected, r);
+  }
+
+  @Test
+  public void testSubtract2() {
+    final Rational r1 = Rational.fromLong(2);
+    final Rational r2 = new Rational(5, 6);
+    final Rational r = r1.subtract(r2);
+    final Rational expected = new Rational(7, 6);
+
+    assertNotNull(r);
+    assertEquals(expected, r);
+  }
+
+  @Test
+  public void testDivide1() {
+    final Rational r1 = new Rational(3, 4);
+    final Rational r2 = new Rational(1, 2);
+    final Rational r = r1.divide(r2);
+    final Rational expected = new Rational(3, 2);
+
+    assertNotNull(r);
+    assertEquals(expected, r);
+  }
+
+  @Test
+  public void testDivide2() {
+    final Rational r1 = Rational.fromLong(4);
+    final Rational r2 = new Rational(3, 4);
+    final Rational r = r1.divide(r2);
+    final Rational expected = new Rational(16, 3);
+
+    assertNotNull(r);
+    assertEquals(expected, r);
+  }
+
+  @Test
+  public void testDivide3() {
+    final Rational r1 = new Rational(1, 2);
+    final Rational r2 = new Rational(1, 4);
+    final Rational r = r1.divide(r2);
+    final Rational expected = new Rational(2, 1);
+
+    assertNotNull(r);
+    assertEquals(expected, r);
+  }
+
+  @Test
+  public void testMultiply1() {
+    final Rational r1 = new Rational(3, 5);
+    final Rational r2 = new Rational(1, 2);
+    final Rational r = r1.multiply(r2);
+    final Rational expected = new Rational(3, 10);
+
+    assertNotNull(r);
+    assertEquals(expected, r);
+  }
+
+  @Test
+  public void testMultiply2() {
+    final Rational r1 = Rational.fromLong(22);
+    final Rational r2 = new Rational(1, 3);
+    final Rational r = r1.multiply(r2);
+    final Rational expected = new Rational(22, 3);
+
+    assertNotNull(r);
+    assertEquals(expected, r);
+  }
+
+  @Test
+  public void testMultiply3() {
+    final Rational r1 = new Rational(1, 2);
+    final Rational r2 = new Rational(1, 4);
+    final Rational r = r1.multiply(r2);
+    final Rational expected = new Rational(1, 8);
+
+    assertNotNull(r);
+    assertEquals(expected, r);
+  }
+
+  @Test
+  public void testAsDouble1() {
+    final Rational r = new Rational(3, 4);
+    final double d = r.asDouble();
+    final double expected = 0.75;
+
+    assertTrue(Double.compare(expected, d) == 0);
+  }
+
+  @Ignore
+  @Test
+  public void createCache() {
+    final Set<String> keys = new HashSet<>();
+
+    int count = 0;
+
+    for (int i = 1; i <= 10; i++) {
+      for (int j = 1; j <= 10; j++) {
+        if (i > j) continue;
+
+        final double resultOfDivision = ((double) i / (double) j);
+        final String resultAsString = Double.toString(resultOfDivision);
+        final int indexOfDecimal = resultAsString.indexOf('.');
+        final String decimalPart = resultAsString.substring(indexOfDecimal + 1);
+
+        if (decimalPart.equals("0")) continue;
+        if (keys.contains(decimalPart)) continue;
+
+        System.out.format(
+          "Map.entry(\"%s\", new Rational(%d, %d)),%n",
+          decimalPart, i, j);
+
+        keys.add(decimalPart);
+        count++;
+      }
+    }
+
+    System.out.format("%nCalculated %d entries.%n", count);
   }
 
 }
